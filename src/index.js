2a37ed1ab3ab3580a224b2150699a0d982ec4b5a
Vlad <vlad09fedko@gmaul.com>
2026-05-08 18:57:04 +0300

    Simplify app

--- src/index.js
+++ src/index.js
@@ -21,14 +21,8 @@ const arrGroups = [
  * The function takes an array of group names, checks for pattern matching, and returns an array of strings with the results
  */
 function checkGroup(arr) {
-  const resultArr = [];
   const reg = /^(F[DME]|f[dme])20\d{2}(-\d+)?$/;
-  for (let i = 0; i < arr.length; i++) {
-    const res = reg.test(arr[i]) ? 'Success' : 'Error';
-    resultArr[i] = `${arr[i]}: ${res}`;
-  }
-
-  return resultArr;
+  return arr.map(group => `${group}: ${reg.test(group) ? 'Success' : 'Error'}`);
 }
 
-console.log(checkGroup(arrGroups));
+console.log(checkGroup(arrGroups)); 
